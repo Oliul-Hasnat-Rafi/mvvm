0e3f5f668da5d4a83674fbbc4557226786c73911
Oliul-Hasnat-Rafi <oliulhasnatrafi270@gmail.com>
2024-01-24 20:57:42 +0600

    main

---
+++ lib/data/Appexception.dart
@@ -0,0 +1,25 @@
+class AppException implements Exception {
+  final _message;
+  final _prefix;
+  AppException([this._message, this._prefix]);
+
+  String toString() {
+    return '$_prefix $_message';
+  }
+}
+
+class FetchException extends AppException {
+  FetchException([String? message]) : super(message, 'Eorrr to comminucation');
+}
+
+class BadRequestexception extends AppException {
+  BadRequestexception([String? message]) : super(message, 'Invild Request');
+}
+
+class unauthexception extends AppException {
+  unauthexception([String? message]) : super(message, 'unauth Request');
+}
+
+class InvildinputException extends AppException {
+  InvildinputException([String? message]) : super(message, 'Invild input');
+}
