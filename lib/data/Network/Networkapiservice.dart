6fe4d2bd599757c4770e6d4229a9ef59d9d81fdc
Oliul-Hasnat-Rafi <oliulhasnatrafi270@gmail.com>
2024-01-25 20:35:49 +0600

    Update network API service to use `http` package and add `postapidata` method (generated by blackbox.ai )

--- lib/data/Network/Networkapiservice.dart
+++ lib/data/Network/Networkapiservice.dart
@@ -1,39 +1,48 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:http/http.dart';
 import 'package:mvvm/data/Appexception.dart';
 import 'package:mvvm/data/Network/baseapiservice.dart';
 import 'package:http/http.dart' as Http;
+
 class NetwrokAApiService implements Baseapiservice {
   @override
   Future getapidata(String url) async {
     dynamic responsejson;
     try {
-      final _response= Http.get(Uri.parse(url)).timeout(Duration(seconds: 30));
-     responsejson = RetuenResponse(_response as Http.Response);
-    }
-    on SocketException {
+      final _response = Http.get(Uri.parse(url)).timeout(Duration(seconds: 30));
+      responsejson = RetuenResponse(_response as Http.Response);
+    } on SocketException {
       throw FetchException('NO Internet');
     }
     return responsejson;
   }
 
   @override
-  Future postapidata(String url) {
-    // TODO: implement postapidata
-    throw UnimplementedError();
+  Future postapidata(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      Response response =
+          await post(Uri.parse(url), body: data).timeout(Duration(seconds: 10));
+
+      responseJson = RetuenResponse(response);
+    } on SocketException {
+      throw FetchException('No Internet Connection');
+    }
+
+    return responseJson;
   }
 
-  dynamic RetuenResponse(Http.Response response)
-  {
-    switch(response.statusCode){
+  dynamic RetuenResponse(Http.Response response) {
+    switch (response.statusCode) {
       case 200:
-        dynamic responsejson= jsonDecode(response.body);
+        dynamic responsejson = jsonDecode(response.body);
         return responsejson;
       case 400:
-          throw BadRequestexception(response.statusCode.toString());
-        default:
-        throw FetchException("Bed Internat"+response.statusCode.toString());
+        throw BadRequestexception(response.statusCode.toString());
+      default:
+        throw FetchException("Bed Internat" + response.statusCode.toString());
     }
   }
 }
--- lib/data/Network/baseapiservice.dart
+++ lib/data/Network/baseapiservice.dart
@@ -1,5 +1,5 @@
 abstract class Baseapiservice {
   Future<dynamic> getapidata(String url);
 
-  Future<dynamic> postapidata(String url);
+  Future<dynamic> postapidata(String url, dynamic data);
 }
