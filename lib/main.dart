c6db1357d6425f10ea6d73e3ff62ed339c8ef791
Oliul-Hasnat-Rafi <oliulhasnatrafi270@gmail.com>
2024-01-26 14:59:06 +0600

    Refactored login screen and view model. (generated by blackbox.ai )

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm/utils/Routes/Routes.dart';
 import 'package:mvvm/utils/Routes/Routes_Name.dart';
+import 'package:mvvm/view_model/auth_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +14,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: RoutesName.Login,
-      onGenerateRoute: Routes.generateRoute,
-    ) ;
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthviewModel()),
+        ChangeNotifierProvider(create: (_) => AuthviewModel())
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: RoutesName.Login,
+        onGenerateRoute: Routes.generateRoute,
+      ),
+    );
   }
 }
-
--- lib/view_model/auth_view_model.dart
+++ lib/view_model/auth_view_model.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm/respository/auth_respository.dart';
+import 'package:mvvm/utils/Routes/Routes_Name.dart';
+import 'package:mvvm/utils/utils.dart';
 
 class AuthviewModel with ChangeNotifier {
   final myrepo = AuthRepository();
 
-  Future<dynamic> login(dynamic userdata) async {
-    myrepo.loginapi(userdata);
+  Future<dynamic> login(dynamic userdata, BuildContext context) async {
+    myrepo.loginapi(userdata).then((value) {
+      Navigator.pushNamed(context, RoutesName.Home);
+      Utils.flushBarErrorMessage('Login Successfully', context);
+    }).onError((error, stackTrace) {
+      Utils.flushBarErrorMessage(error.toString(), context);
+    });
   }
 }
